09cbdae232bd45bac75882c3d21fd0736c5bad3b
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2023-07-11 18:40:24 -0400

    feat: Import argparse and add script arguments

--- picker.py
+++ picker.py
@@ -1,5 +1,14 @@
+import argparse
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path')
+    parser.add_argument('-d','--deep', action='store_true')
+    parser.add_argument('-f','--flat', action='store_true')
+
+    args = parser.parse_args()
+    print(args)
 
 
 if __name__ == '__main__':
