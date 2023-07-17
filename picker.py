a8cda5e68bff9a616a6a992c8fe1d63f4b23f3eb
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2023-07-17 16:11:22 -0400

    feat: Implement weighted sampling

--- picker.py
+++ picker.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+import random
+import yaml
 
 
 def get_arguments():
@@ -23,9 +26,34 @@ def get_arguments():
         )
 
 
+def weighted_pick(node):
+    while os.path.isdir(node):
+        pick = random.choice(os.listdir(node))
+        node = os.path.join(node, pick)
+    print(node)
+
+    with open(node) as file:
+        node = random.choice(yaml.safe_load(file))
+    print(node)
+
+    while type(node) is dict or type(node) is list:
+        if type(node) is dict:
+            for k, v in node.items():
+                print(k, end=' -> ')
+                node = v
+
+        if type(node) is list:
+            node = random.choice(node)
+    print(node)
+
+
 def main():
     node, mode = get_arguments()
     print(node, mode)
+
+    match mode:
+        case 'weighted':
+            weighted_pick(node)
 
 
 if __name__ == '__main__':
