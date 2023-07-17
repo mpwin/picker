771c781e1c48989c662f96b67a23c281fe8c3b56
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2023-07-17 15:19:41 -0400

    refactor: Add get_arguments() method

--- picker.py
+++ picker.py
@@ -1,14 +1,23 @@
 import argparse
 
 
-def main():
+def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('path')
     parser.add_argument('-d','--deep', action='store_true')
     parser.add_argument('-f','--flat', action='store_true')
 
-    args = parser.parse_args()
-    print(args)
+    arguments = parser.parse_args()
+
+    return (
+        arguments.path,
+        'deep' if arguments.deep else 'flat',
+        )
+
+
+def main():
+    node, mode = get_arguments()
+    print(node, mode)
 
 
 if __name__ == '__main__':
